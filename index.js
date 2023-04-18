4b3c8647e37284fddb305e0ed581050720ae0c07
Cole Perry <cperry30@jh.edu>
2023-04-17 23:19:35 -0400

    Reporting the end of game is dynamic now

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const path = require('node:path');
 // Require the necessary discord.js classes
 const { Client, Collection, Events, GatewayIntentBits, messageLink } = require('discord.js');
 const { token } = require('./config.json');
+const { teamList } = require('./teamList');
 
-
+// Set up team list
+const teamIDs = [];
+for (let team of teamList) {
+	teamIDs.push(team.reaction);
+  }
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions] });
@@ -14,6 +19,8 @@ const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 let voterArray1 = [];
 let voterArray2 = [];
+let reaction1 = null;
+let reaction2 = null;
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
@@ -51,25 +58,31 @@ client.on(Events.InteractionCreate, async interaction => {
 
 client.on('messageReactionAdd', (reaction, user) => {
 	if (!reaction.message?.reactions.cache.find(v => v.emoji.id === '1086474462834200687')) { // If the stop emoji doesnt yet exist
-		if(reaction.emoji.id === "1014887969808711800") {
-			if (user.username !== 'IteroBetBot') {
-				voterArray1.push(user.username);
+		if (teamIDs.includes(reaction.emoji.id)) {
+			if(!reaction1) {
+				reaction1 = reaction.emoji.id;
+			} else if (!reaction2) {
+				reaction2 = reaction.emoji.id;
+			} 
+			if (reaction.emoji.id === reaction1) {
+				if (user.username !== 'IteroBetBot') {
+					voterArray1.push(user.username)
+				}
+			} else if (reaction.emoji.id === reaction2) {
+				if (user.username !== 'IteroBetBot') {
+					voterArray2.push(user.username)
+				}
 			}
-		} else if (reaction.emoji.id === "1065285604578959430") {
-			if (user.username !== 'IteroBetBot') {
-				voterArray2.push(user.username);
-			}
+			console.log (voterArray1, voterArray2);
 		}
-
-		// For some reason my name, and only my name, is being added twice here.
-
 	}
+
 	if (reaction.emoji.id === '1086474462834200687') { //If stop emoji
 		if (user.id != '235113888973062155') { // If the user who added the stop emoji isnt Admin then remove it
 		reaction.message.reactions.cache.get('1086474462834200687').remove()
 			.catch(error => console.error('Failed to remove reactions:', error));
 
-			// Problem right now is that if Collin has added the stop emoji and then I try to add it again it removes both of ours, not just mine.
+		// Problem right now is that if Collin has added the stop emoji and then I try to add it again it removes both of ours, not just mine.
 
 		} else {
 		console.log('Array1', voterArray1);
@@ -82,8 +95,10 @@ client.on('messageReactionAdd', (reaction, user) => {
 		 voterArray2.forEach(element => {
 			finalString2 += element + " \n"
 		});
-		finalString1 += 'Voted for Fnatic ' + '<:FNC:1014887969808711800>';
-		finalString2 += 'Voted for Koi ' + '<:KOI:1065285604578959430>';
+		const team1 = teamList.find(team => team.reaction === reaction1);
+		const team2 = teamList.find(team => team.reaction === reaction2);
+		finalString1 += 'Voted for ' + team1.name + ' ' + team1.emoji;
+		finalString2 += 'Voted for ' + team2.name + ' ' + team2.emoji;
 		client.channels.cache.get('1077612967639666738').send(finalString1);
 		client.channels.cache.get('1077612967639666738').send(finalString2);
 		}
