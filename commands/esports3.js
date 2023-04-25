19b0042921cd0fc01d264dc7cde98c6e74200212
Cole Perry <cperry30@jh.edu>
2023-04-24 21:29:16 -0400

    Esports Predictors messages working fine

--- commands/esports3.js
+++ commands/esports3.js
@@ -12,11 +12,11 @@ module.exports = {
 		.setDescription('Sends the esports3 message'),
 	async execute(interaction) {
         await interaction.reply({content: 'Message sent.', fetchReply: true});
-        const message = '@Esports Predictors' + '\n' + '\n' + 
-        'If  you would like to participate in our Esports Predictions, and have a chance to win LoL skins, react to each above message with the logo of the team you predict to win the game. Once you see a :rammus_ok:  reaction on the game, picks are locked in, and any additional votes will not be taken into account. For each correct prediction, you will receive a point on the leaderboard. The winner of predictions at the end of each Split or Tournament will win a free LoL Skin (EU/NA Servers only).' + '\n' +
+        const message = '<@&1074582889901068288>' + '\n' + '\n' + 
+        'If  you would like to participate in our Esports Predictions, and have a chance to win LoL skins, react to each above message with the logo of the team you predict to win the game. Once you see a <:rammus_ok:1086474462834200687> reaction on the game, picks are locked in, and any additional votes will not be taken into account. For each correct prediction, you will receive a point on the leaderboard. The winner of predictions at the end of each Split or Tournament will win a free LoL Skin (EU/NA Servers only).' + '\n' +
         '\n' +
         'Since these matches are a 5 game series, you can receive a bonus point if you correctly predict the winner AND how many games the series will go (:three:, :four: or :five:). ' + '\n' + '\n' +
-        'If you would like to receive pings when predictions are  posted, you can go to Customize Community to be added to the Esports Predictors role.';
+        'If you would like to receive pings when predictions are  posted, you can go to <id:customize> to be added to the Esports Predictors role.';
         client.channels.cache.get('1077612967639666738').send(message);
 	},
 };
