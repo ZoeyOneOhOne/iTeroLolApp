3d5f82efbddf67a5bb1c5ef5e7746ee5bbb7e2f0
kingshmiley <40242669+kingshmiley@users.noreply.github.com>
2023-04-28 16:43:44 -0400

    Changed to MSI Teams

--- teamList.js
+++ teamList.js
@@ -1,14 +1,17 @@
 const teamList = [
-	{ name: 'Fnatic', value: 'Fnatic', reaction: '1014887969808711800', emoji: '<:FNC:1014887969808711800>'},
-	{ name: 'G2 Esports', value: 'G2 Esports', reaction: '1014887970890842233', emoji: '<:G2:1014887970890842233>'},
-	{ name: 'Team VItality', value: 'Team VItality', reaction: '1012762647134994562', emoji: '<:VIT:1012762647134994562>'},
-    { name: 'Team BDS', value: 'Team BDS', reaction: '1014887968512692324', emoji: '<:BDS:1014887968512692324>'},
-	{ name: 'Excel', value: 'Excel', reaction: '1014887979354964059', emoji: '<:XL:1014887979354964059>'},
-	{ name: 'Astralis', value: 'Astralis', reaction: '1014887966331646035', emoji: '<:AST:1014887966331646035>'},
-    { name: 'SK Gaming', value: 'SK Gaming', reaction: '1014887976813219942', emoji: '<:SK:1014887976813219942>'},
-	{ name: 'KOI', value: 'KOI', reaction: '1065285604578959430', emoji: '<:KOI:1065285604578959430>'},
-	{ name: 'MAD Lions', value: 'MAD Lions', reaction: '1014887972266578000', emoji: '<:MAD:1014887972266578000>'},
-    { name: 'Team Heretics', value: 'Team Heretics', reaction: '1065287618939600917', emoji: '<:TH:1065287618939600917>'},
+	{ name: 'Bilibili Gaming', emoji: '<:BLG:1012798051288625294>'},
+	{ name: 'JDG Gaming', emoji: '<:JDG:1012798055080271972>'},
+	{ name: 'Gen.G', emoji: '<:GEN:1012798384710623244> '},
+	{ name: 'T1', emoji: '<:T1:1012798391799009350>'},
+	{ name: 'Cloud9', emoji: '<:C9:1012798298215698515>'},
+	{ name: 'Golden Guardians', emoji: '<:GG:1012798304146423958>'},
+	{ name: 'G2 Esports', emoji: '<:G2:1014887970890842233>'},
+	{ name: 'MAD Lions', emoji: '<:MAD:1014887972266578000>'},
+	{ name: 'GAM Esports', emoji: '<:GAM:1100074924900626452>'},
+	{ name: 'Movistar R7', emoji: '<:R7:1100074928084099204> '},
+	{ name: 'PSG Talon', emoji: '<:PSG:1100074926830002206> '},
+	{ name: 'LOUD', emoji: '<:LOUD:1100074359592341635>'},
+	{ name: 'DetonatioN FocusMe', emoji: '<:DFM:1100074356685680650>'},
 ]
-
+// { name: 'NAME', emoji: 'EMOJI'},
 exports.teamList = teamList;
