7fa5ebd44576af17a22ac0f62a83d8f2adf98db1
ZoeyOneOhOne <cole.perry113@gmail.com>
2023-08-18 16:46:37 -0400

    Reporting games works. It only accounts for which team won and increments their score by 1.

---
+++ commands/reportGame.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { reportGame } = require('../db');
+const wait = require('node:timers/promises').setTimeout;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('reportgame')
+		.setDescription('Report the result of a game.')
+        .addStringOption(option => 
+            option.setName('input')
+                .setDescription('Message ID')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName('winner')
+                .setDescription('Winner')
+                .setRequired(true)
+			    .addChoices(
+                    { name: 'Bilibili Gaming', value: 'Bilibili Gaming'},
+                    { name: 'JDG Gaming', value: 'JDG Gaming'},
+                    { name: 'Gen.G', value: 'Gen.G'},
+                    { name: 'T1', value: 'T1'},
+                    { name: 'Cloud9', value: 'Cloud9'},
+                    { name: 'Golden Guardians', value: 'Golden Guardians'},
+                    { name: 'G2 Esports', value: 'G2 Esports'},
+                ))
+        .addStringOption(option =>
+            option.setName('games')
+                .setDescription('Number of games')
+                .setRequired(true)
+                .addChoices(
+                    { name: '1', value: '1'},
+                    { name: '3', value: '3'},
+                    { name: '5', value: '5'},
+                )),
+	async execute(interaction) {
+        const messageId = interaction.options.getString('input'); 
+        const winner = interaction.options.getString('winner'); 
+        const games = interaction.options.getString('games');
+
+        await reportGame(winner, messageId, games);
+
+
+
+        await interaction.reply(winner + ' won '+ games +' games in the series.');
+	},
+};
--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { firebaseConfig } = require('./firebaseConfig');
 const { initializeApp } = require('firebase/app');
-const { getFirestore, collection, getDocs, doc, setDoc, updateDoc } = require('firebase/firestore/lite');
+const { getFirestore, collection, getDocs, doc, getDoc, setDoc, updateDoc, query, where, increment } = require('firebase/firestore/lite');
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
@@ -30,7 +30,6 @@ async function seriesVote(series, username, messageID) {
 async function addGame(team1, team2, messageID) {
 	data = {
 		result: 'pending',
-		isLocked: false,
 		numberOfGames: 3,
 		team1: team1,
 		team2: team2
@@ -39,7 +38,77 @@ async function addGame(team1, team2, messageID) {
 	await setDoc(userVoteDocRef, data);
 }
 
+async function reportGame(result, messageId, games) {
+	const gamesCollectionRef = collection(db, 'Games');
+	const docRef = doc(gamesCollectionRef, messageId);
+	const updateData =  {
+		result: result,
+		games: games
+	}
+
+	try {
+		await updateDoc(docRef, updateData);
+		console.log('Game reported successfully');
+
+		const userList = await getUsersWhoVotedCorrectly(messageId, result);
+		await updateScoreboard(userList);
+	} catch (error) {
+		console.error('Error reporting game:', error);
+	}
+}
+
+
+
+async function getUsersWhoVotedCorrectly(messageId, winner) {
+	return new Promise(async (resolve, reject) => {
+		const gameDocumentRef = doc(collection(db, 'Games'), messageId);
+		const votesCollectionRef = collection(gameDocumentRef, 'Votes');
+	
+		try {
+		  const queryVotedFor = query(votesCollectionRef, where('votedFor', '==', winner));
+		  const querySnapshot = await getDocs(queryVotedFor);
+	
+		  const usernames = querySnapshot.docs.map(doc => doc.id);
+		  console.log('Usernames with matching votedFor value:', usernames);
+		  
+		  resolve(usernames);
+		} catch (error) {
+		  console.error('Error querying documents:', error);
+		  reject(error);
+		}
+	  });
+}
+
+async function updateScoreboard(userList) {
+	const scoreRef = collection(db, 'Scoreboard');
+
+	try {
+		for (const username of userList) {
+		  const userDocRef = doc(scoreRef, username);
+		  
+		  const userDocSnapshot = await getDoc(userDocRef);
+	
+		  if (userDocSnapshot.exists()) {
+			// Document exists, update it
+			await updateDoc(userDocRef, {
+			  points: increment(1) // Increment the points field by 1
+			});
+			console.log(`Updated scoreboard for ${username}`);
+		  } else {
+			// Document doesn't exist, create a new one
+			await setDoc(userDocRef, {
+				points: 1 // Initialize the points field
+			});
+			console.log(`Created new scoreboard emtry for ${username}`);
+		  }
+		}
+	  } catch (error) {
+		console.error('Error updating scoreboard:', error);
+	  }
+}
+
 exports.getTeams = getTeams;
 exports.castVote = castVote;
 exports.addGame = addGame;
 exports.seriesVote = seriesVote;
+exports.reportGame = reportGame;
