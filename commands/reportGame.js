c2721b7adab35e114a63cd5234fcb8a978f6bb9f
ZoeyOneOhOne <cole.perry113@gmail.com>
2023-08-18 16:52:31 -0400

    Housekeeping

--- commands/reportGame.js
+++ commands/reportGame.js
@@ -29,8 +29,9 @@ module.exports = {
                 .setDescription('Number of games')
                 .setRequired(true)
                 .addChoices(
-                    { name: '1', value: '1'},
+                    { name: '2', value: '2'},
                     { name: '3', value: '3'},
+                    { name: '4', value: '4'},
                     { name: '5', value: '5'},
                 )),
 	async execute(interaction) {
